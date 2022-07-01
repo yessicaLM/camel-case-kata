987b224c6e997a0570d03ff7cce6dd5ed441dd7f
yessica <yessica@leanmind.es>
2022-07-01 08:22:15 +0100

    RED: sequence with empty spaces and symbols returns capitalized words

--- src/tests/camelCase.test.ts
+++ src/tests/camelCase.test.ts
@@ -4,7 +4,7 @@ import { convertToCamelCase } from '../core/camelCase';
 // 'Foo' -> 'Foo' --> DONE
 // 'Foo Bar' -> 'FooBar' --> DONE
 // 'Foo_Bar-Foo' -> 'FooBarFoo'
-// 'foo' -> 'Foo'
+// 'foo' -> 'Foo' --> DONE
 // 'Foo__bar foo-bar' -> 'FooBarFooBar'
 
 describe('Camel case', () => {
@@ -22,5 +22,9 @@ describe('Camel case', () => {
 
   it('_ and - are removed from separated words', () => {
     expect(convertToCamelCase('Foo_Bar-Foo')).toBe('FooBarFoo');
+  });
+
+  it('sequence with empty spaces and symbols returns capitalized words', () => {
+    expect(convertToCamelCase('Foo__bar foo-bar')).toBe('FooBarFooBar');
   });
 });
