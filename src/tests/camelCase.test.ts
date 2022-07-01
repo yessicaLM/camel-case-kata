b38974f7bc0a6bd1e6ffc5f1fb3eb270546fb924
yessica <yessica@leanmind.es>
2022-07-01 08:17:55 +0100

    RED: _ and - are removed from separated words

--- src/tests/camelCase.test.ts
+++ src/tests/camelCase.test.ts
@@ -2,7 +2,7 @@ import { convertToCamelCase } from '../core/camelCase';
 
 // "" -> "" --> DONE
 // 'Foo' -> 'Foo' --> DONE
-// 'Foo Bar' -> 'FooBar'
+// 'Foo Bar' -> 'FooBar' --> DONE
 // 'Foo_Bar-Foo' -> 'FooBarFoo'
 // 'foo' -> 'Foo'
 // 'Foo__bar foo-bar' -> 'FooBarFooBar'
@@ -18,5 +18,9 @@ describe('Camel case', () => {
 
   it('two capitalized word separated by empty space return FooBar', () => {
     expect(convertToCamelCase('Foo Bar')).toBe('FooBar');
+  });
+
+  it('_ and - are removed from separated words', () => {
+    expect(convertToCamelCase('Foo_Bar-Foo')).toBe('FooBarFoo');
   });
 });
