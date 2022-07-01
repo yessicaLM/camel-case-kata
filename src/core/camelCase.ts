e2f810069e956f58d34ad5252a732081299e55bb
yessica <yessica@leanmind.es>
2022-07-01 08:26:29 +0100

    GREEN: sequence with empty spaces and symbols returns capitalized words

--- src/core/camelCase.ts
+++ src/core/camelCase.ts
@@ -1,8 +1,14 @@
 export const convertToCamelCase = (value: string): string => {
   if (value.charAt(0) === value.charAt(0).toUpperCase()) {
-    const valueWithoutSpaces = value.split(' ').join('');
-    const removedSymbols = valueWithoutSpaces.split(/[_-]/).join('');
-    return removedSymbols;
+    const removedSymbols = value.split(/[_-\s]/);
+
+    const capitalizedWords = [];
+    removedSymbols.forEach(element => {
+      const firstLetter = element.charAt(0);
+      const word = element.substring(1);
+      capitalizedWords.push(`${firstLetter.toUpperCase()}${word}`)
+    });
+    return capitalizedWords.join('');
   }
   return '';
 }
