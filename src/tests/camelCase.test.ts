4458bc87f814de4eec2ea4a5e88a10f8616f65ce
yessica <yessica@leanmind.es>
2022-07-01 08:08:35 +0100

    RED: capitalize single word returns same word without changes

--- src/tests/camelCase.test.ts
+++ src/tests/camelCase.test.ts
@@ -1,6 +1,6 @@
 import { convertToCamelCase } from '../core/camelCase';
 
-// "" -> ""
+// "" -> "" --> DONE
 // 'Foo' -> 'Foo'
 // 'Foo Bar' -> 'FooBar'
 // 'Foo_Bar-Foo' -> 'FooBarFoo'
@@ -10,5 +10,9 @@ import { convertToCamelCase } from '../core/camelCase';
 describe('Camel case', () => {
   it('empty strings are allowed', () => {
     expect(convertToCamelCase('')).toBe('');
+  });
+
+  it('capitalize single word returns same word without changes', () => {
+    expect(convertToCamelCase('Foo')).toBe('Foo');
   });
 });
